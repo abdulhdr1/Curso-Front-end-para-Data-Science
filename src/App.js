c5b5c08b8a40cc6e9c2b75ae55672006bb605d1d
Abdul <abdulhdr1@gmail.com>
2022-04-15 16:08:16 -0300

    Aula 2
    Estruturação do layout com componentes do Material UI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import "./App.css";
 import Sidebar from "./components/Sidebar";
+import Image from "./components/Image";
+import { Container, Grid } from "@mui/material";
+import { colors } from "./styles";
+import { Header } from "./components/Header";
+import { DataGrid } from "./components/DataGrid";
 
 const logo =
   "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/640px-React-icon.svg.png";
@@ -7,21 +12,23 @@ const logo =
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code></code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Container
+        sx={{
+          width: "100vw",
+          height: "100vh",
+          display: "flex",
+          maxWidth: "100vw!important",
+          padding: "0!important",
+          background: "yellow",
+        }}
+      >
         <Sidebar />
-      </header>
+        <Grid container>
+          <Header />
+
+          <DataGrid />
+        </Grid>
+      </Container>
     </div>
   );
 }
--- src/components/SidebarItem/index.js
+++ src/components/SidebarItem/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { ListItemButton, ListItemIcon } from "@mui/material";
-import { FilterAlt } from "@mui/icons-material";
 
 export default function SidebarItem(props) {
   return (
